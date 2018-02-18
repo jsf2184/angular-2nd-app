ead05bd4694b0c61ff753a5453bf4a1430ff52d5
jsf2184 <jsf2184@gmail.com>
2018-02-18 10:32:41 -0600

    Add exercises component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import {WarningComponent} from './warning/warning.component';
 import { SuccessComponent } from './success/success.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
 
 
@@ -17,7 +18,8 @@ import { SuccessComponent } from './success/success.component';
     ServerComponent,
     WarningComponent,
     ServersComponent,
-    SuccessComponent
+    SuccessComponent,
+    ExercisesComponent
   ],
   imports: [
     BrowserModule,
